9e43aa8e4262d63fd7a4a7c09b61dec8e7fe752e
Viktor Pyskunov <vicpi89@gmail.com>
2019-11-22 17:24:06 +0200

    Add request helper

---
+++ src/helpers/request.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+/**
+ * Simulates delay
+ * 
+ * @param {Number} ms Delay in milliseconds
+ * @returns {Promise}
+ */
+const delay = (ms) => {
+    return new Promise(resolve => 
+        setTimeout(resolve, ms)
+    )
+}
+
+/**
+ * Wrapper around axios.get method in order to simulate slow network connection
+ * 
+ * @param {String} url URL
+ */
+const get = async (url) => {
+    await delay(2000)
+    let response
+    try {
+        response = await axios.get(url)
+    } catch (err) {
+        console.log('HTTP Request Error: ', err, err.stack)
+    }
+    return response
+}
+
+export default {
+    get
+}
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,13 +1,14 @@
 import { LOAD_INITIAL_USERS, SCROLL_AT_THE_BOTTOM_OF_THE_PAGE, addUsers, setSeed, setPage } from 'actions'
 import { call, put, takeLeading, all, select } from 'redux-saga/effects'
-import { makeUsersUrl, httpGet } from 'helpers'
+import { makeUsersUrl } from 'helpers'
+import request from 'helpers/request'
 
 function* loadInitialUsersSaga() {
     console.log('loadInitialUsersSaga');
     const nationalities = yield select(state => state.nationalities)
     console.log('nationalities', nationalities);
     
-    const response = yield call(httpGet, makeUsersUrl(nationalities))
+    const response = yield call(request.get, makeUsersUrl(nationalities))
     const users = response.data.results
     const seed = response.data.info.seed
     yield put(addUsers(users))
@@ -21,7 +22,7 @@ function* addUsersSaga() {
     const currentPage = yield select(state => state.page)
     const nextPage = currentPage + 1
     console.log('addUsersSaga', seed, nextPage);
-    const response = yield call(httpGet, makeUsersUrl(nationalities, seed, nextPage))
+    const response = yield call(request.get, makeUsersUrl(nationalities, seed, nextPage))
     const users = response.data.results
     yield put(addUsers(users))
     yield put(setPage(nextPage))
