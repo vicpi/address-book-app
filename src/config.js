a4fb6ac87b897e9b181e985df125e7133ca3d3f4
Viktor Pyskunov <vicpi89@gmail.com>
2019-11-25 12:37:41 +0200

    Adjust API_URL to use local backend

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export const BATCH_SIZE = 50
-export const MAX_USERS = 1000
-export const API_URL = 'https://randomuser.me/api/'
+// TODO: change to 1000
+export const MAX_USERS = 200
+// TODO: change API_URL
+export const API_URL = 'http://localhost:8000'
+// export const API_URL = 'https://randomuser.me/api/'
