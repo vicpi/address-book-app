e884b1cac27d6f1fdad147502ea22924b4fc289c
Viktor Pyskunov <vicpi89@gmail.com>
2019-11-30 12:49:48 +0200

    Update config data

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const BATCH_SIZE = 50
 // TODO: change to 1000
-export const MAX_USERS = 200
+export const MAX_USERS = 1000
 // TODO: change API_URL
-export const API_URL = 'http://localhost:8000'
-// export const API_URL = 'https://randomuser.me/api/'
+// export const API_URL = 'http://localhost:8000'
+export const API_URL = 'https://randomuser.me/api/'
