8a70b2ddbabb6255873e63ce5bba9f611eecae90
Viktor Pyskunov <vicpi89@gmail.com>
2019-12-01 11:47:16 +0200

    Read port from ENV

--- server/server-prod.js
+++ server/server-prod.js
@@ -11,7 +11,7 @@ import App from 'components/App/App'
 const routes = require('./routes/index')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use('/static', express.static(path.join(__dirname, '..', 'dist')));
 
